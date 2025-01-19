d7e00275aad8189323e52ce1e7ad82fd32c32bd3
shishirRsiam <shishir.siam01@gmail.com>
2025-01-19 17:40:39 +0600

    Update Code and Add Readme.md

---
+++ Windows Start Time/README.md
@@ -0,0 +1,2 @@
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=shishirrsiam&label=Profile%20views&color=0e75b6&style=flat" alt="shishirrsiam" /> </p>
+
--- Windows Start Time/auto_commit_push_with_timestamp.py
+++ Windows Start Time/auto_commit_push_with_timestamp.py
@@ -2,29 +2,29 @@ from pathlib import Path
 import time, os, subprocess
 from datetime import datetime
 
-def try_to_push(commit_message, can_try):
+def try_to_push(commit_message, retry_flag):
     try:
-        subprocess.run(['git', 'add', '.'], check=True)
-        subprocess.run(['git', 'commit', '-m', commit_message], check=True)
+        subprocess.run(['git', 'add', '.'], check=True) 
+        subprocess.run(['git', 'commit', '-am', commit_message], check=True)
         subprocess.run(['git', 'push'], check=True)
-        can_try = False
+        retry_flag = False
     except subprocess.CalledProcessError as e:
         print(f"Error occurred during git operation: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
-    if can_try:
+    if retry_flag:
         time.sleep(60 * 2)  # Wait for 2 minutes
         try_to_push(commit_message, False)
     
 
 def prepare_for_git_push(file_name):
-    commit_message = f'Update WIN_Start_Time: {file_name}'
+    commit_message = f'Update Windows Start Time: {file_name}'
     try_to_push(commit_message, True)
 
 
 def create_file(current_time, folder_name, file_name):
-    Path(folder_name).mkdir(exist_ok=True)
+    Path(folder_name).mkdir(exist_ok=True) 
     file_path = Path(folder_name) / file_name
     try:
         with open(file_path, 'r') as file:
@@ -40,17 +40,16 @@ def create_file(current_time, folder_name, file_name):
 def process():
     current = datetime.now()
 
-    day = current.strftime('%d')
-    year = current.strftime('%Y')
-    day_name = current.strftime('%A')
-    month_name = current.strftime('%B')
-    month_count = current.strftime('%m')
+    day = current.strftime('%d') # day of the month. eg: 1-31
+    year = current.strftime('%Y') # year. eg: 2025
+    day_name = current.strftime('%A') # day of the week. eg: Sunday-Saturday
+    month_name = current.strftime('%B') # month of the year. eg: January-December
+    month_count = current.strftime('%m') # month count. eg: 1-12
 
-    current_time = current.strftime('%H:%M:%S %p\n')
-    folder_name = f"{month_count} {month_name} {year}"
-    file_name = f"{day} {month_name} ({day_name}).txt"
-
-    return create_file(current_time, folder_name, file_name)
+    current_time = current.strftime('%I:%M:%S %p\n') # 12-hour format with AM/PM. eg: 06:13:35 AM
+    folder_name = f"{month_count} {month_name} {year}" # eg: 01 January 2025
+    file_name = f"{day} {month_name} ({day_name}).txt" # eg: 01 January (Sunday).txt
+    return create_file(current_time, folder_name, file_name) 
 
 
 if __name__ == "__main__":
